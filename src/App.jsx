19cbad208988f62987a638b1c6a1d6547de2fb91
Israel Teixeira <israeltx@gmail.com>
2026-02-10 17:57:47 -0300

    Create shoe detail layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Nav } from "./components/Nav";
+import { ShoeDetail } from "./components/ShoeDetail";
 
 export default function App() {
   return (
     <div className="p-10 xl:px-24">
-      <Nav></Nav>
+      <Nav />
+      <ShoeDetail />
     </div>
   );
 }
