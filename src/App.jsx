70c319f4acf97a4f6d86026ed1f1936c17c9c19e
Israel Teixeira <israeltx@gmail.com>
2026-02-12 11:21:50 -0300

    Create the dark button

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { BiSun } from "react-icons/bi"; 
+import { BiMoon } from "react-icons/bi"; 
 import { NewArrivalsSection } from "./components/NewArrivalsSection";
 import { ShoeDetail } from "./components/ShoeDetail";
 import { Sidebar } from "./components/Sidebar";
@@ -17,6 +19,14 @@ const FAKE_CART_ITEMS = SHOE_LIST.map(shoe => {
 export default function App() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false)
 
+  const toggleDarkMode = () => {
+    window.document.documentElement.classList.toggle("dark");
+    localStorage.setItem(
+      "isDarkMode",
+      window.document.documentElement.classList.contains("dark"),
+    );
+  };
+
   return (
     <div className="animate-fadeIn p-10 xl:px-24">
       <Nav onClickShoppingBtn={()=> setIsSidebarOpen(true)}/>
@@ -25,6 +35,15 @@ export default function App() {
       <Sidebar isOpen={isSidebarOpen} onClickClose={() => setIsSidebarOpen(false)}>
         <Cart cartItems={FAKE_CART_ITEMS}/>
       </Sidebar>
+      <div className="fixed bottom-4 right-4">
+        <button
+          onClick={toggleDarkMode}
+          className="bg-night-50 dark:text-night rounded-full px-4 py-2 text-white shadow-lg dark:bg-white cursor-pointer"
+        >
+          <BiSun className="hidden dark:block" />
+          <BiMoon className="dark:hidden" />
+        </button>
+      </div>
     </div>
   );
 }
